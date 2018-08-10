6016842632fffd441bdf5065c33794d53c1361e4
Jose Manuel Cardona <josemanuel.cardona@softonic.com>
2018-08-10 09:01:49 +0200

    master Register event listener in service provider to allow automatic dataset update

--- src/ScraperProvider.php
+++ src/ScraperProvider.php
@@ -2,13 +2,35 @@
 
 namespace Softonic\LaravelIntelligentScraper;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 use Softonic\LaravelIntelligentScraper\Scraper\Application\XpathBuilder;
+use Softonic\LaravelIntelligentScraper\Scraper\Events\Scraped;
+use Softonic\LaravelIntelligentScraper\Scraper\Listeners\UpdateDataset;
 
-class ScraperProvider extends ServiceProvider
+class ScraperProvider extends EventServiceProvider
 {
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        Scraped::class => [
+            UpdateDataset::class,
+        ],
+    ];
+
+    /**
+     * The subscriber classes to register.
+     *
+     * @var array
+     */
+    protected $subscribe = [];
+
     public function boot()
     {
+        parent::boot();
+
         $this->publishes(
             [__DIR__ . '/config/scraper.php' => config_path('scraper.php')],
             'config'
