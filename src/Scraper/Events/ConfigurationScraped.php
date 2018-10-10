e25ae7c1117923a73be0752a5d8d7f7fc69e7528
Jose Manuel Cardona <josemanuel.cardona@softonic.com>
2018-10-10 15:28:13 +0200

    feature/Improve-dataser-handling-in-configuration-process Fix style

--- src/Scraper/Events/ConfigurationScraped.php
+++ src/Scraper/Events/ConfigurationScraped.php
@@ -2,9 +2,6 @@
 
 namespace Softonic\LaravelIntelligentScraper\Scraper\Events;
 
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
 class ConfigurationScraped extends Scraped
 {
 }
